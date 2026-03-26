5fe2a38d74b17ad0073abb2c1e64c558937cd819
dipenarathod <30725272+dipenarathod@users.noreply.github.com>
2026-03-26 17:06:04 +0530

    Added Input-Output-Helper Library

---
+++ Ada Files/input_output_helper/src/input_output_helper-utils.ads
@@ -0,0 +1,12 @@
+package Input_Output_Helper.Utils is
+
+   function Pack_Four_Bytes (B0, B1, B2, B3 : Unsigned_Byte) return Word;
+   procedure Unpack_Four_Bytes (W : Word; B0, B1, B2, B3 : out Unsigned_Byte);
+   procedure Create_Word_Array_From_Integer_Array
+     (Integer_Source : in Integer_Array; Result_Word_Array : out Word_Array);
+
+   --Pragmas
+   pragma Inline(Pack_Four_Bytes);
+   pragma Inline(Unpack_Four_Bytes);
+   pragma Inline(Create_Word_Array_From_Integer_Array);
+end Input_Output_Helper.Utils;
